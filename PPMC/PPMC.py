3edb5f682a7bd9b35f59e81b8d6df494b3cfe0ee
czarnia <czarniana@gmail.com>
2016-11-20 20:02:08 -0300

    PPMC vs 2.0 (objetoso)

--- PPMC/PPMC.py
+++ PPMC/PPMC.py
@@ -3,26 +3,38 @@ from Context import Context
 from Model import Model
 from BaseModel import BaseModel
 
-def ppmc(text, order, log_file='PPMC.log'):
-    logging.basicConfig(filename=log_file,level=logging.DEBUG)
-    text = text.lower()
-    interval = (0,1)
-    models = [BaseModel()]
-    try:
-        for i in xrange(0, order+1):
-            models.append(Model(i))
-        for pos in xrange(0, len(text)):
-            compressed = False
-            modelNum = order+1
-            while not compressed:
-                compression = models[modelNum].compress(text, pos, [], interval)
-                compressed = compression[0]
-                interval = compression[1]
-                modelNum -= 1
-        logging.info("FINISHED RUNNING PPMC. RETURNED INTERVAL:" +"("+str(interval[0])+","+str(interval[1])+")")
-    except Exception,e:
-        logging.exception(str(e))
-    return interval
+class PPMC(object):
+
+    def __init__(self, order, m):
+        self.order = order
+        if (not m):
+            self.models = [BaseModel()]
+            for i in xrange(0, order+1):
+                models.append(Model(i))
+        else:
+            self.models = m
+
+    def compress(text, log_file='PPMC.log'):
+        logging.basicConfig(filename=log_file,level=logging.DEBUG)
+        text = text.lower()
+        interval = (0,1)
+        try:
+            for pos in xrange(0, len(text)):
+                compressed = False
+                modelNum = self.order+1
+                while not compressed:
+                    compression = self.models[modelNum].compress(text, pos, [], interval)
+                    compressed = compression[0]
+                    interval = compression[1]
+                    modelNum -= 1
+            logging.info("FINISHED RUNNING PPMC. RETURNED INTERVAL:" +"("+str(interval[0])+","+str(interval[1])+")")
+        except Exception,e:
+            logging.exception(str(e))
+        return interval
+
+    def models(self):
+        return self.models
+
 
 FIVE_STARS_TEXT = "i got them in a very timely manner and they re all very large and green. there seems to be an inordinate amount of the seeds e out a weird beige color, but it still tastes great and is a much better deal than any of the indian markets in town. cheers to frontier."
 ONE_STAR_TEXT = "check out the sugar content before you buy!  27 grams/box - the same amount that's in a 12 oz. can of coke!  this is ridiculously high for something that you think is going to be healthy!"
@@ -32,10 +44,11 @@ ANOTHER_ONE_STAR = "this is one of the worse tasting soups i've ever eaten. i ha
 TWO_STAR_REVIEW = "mccann's steel cut oatmeal is the perfect breakfast for people in a hurry.  all you do is put 1/2 cup of oats in a little crockpot with 2 cups of boiling water before you go to bed, then an hour before i get up the crockpot starts on high on a timer so when i get out of the shower the oatmeal is ready to go.  just mix in some blueberries, raisins, craisins, dates, nuts or whatever you want and eat."
 
 def main():
-    #print "PPMC first 5 stars review:" + str(ppmc(FIVE_STARS_TEXT,4,'PPMC_5.log'))
-    #print "PPMC second 5 stars review:" + str(ppmc(ANOTHER_FIVE_STAR,4,'PPMC_5(2).log'))
-    #print "PPMC first 1 star review:" + str(ppmc(ONE_STAR_TEXT,4,'PPMC_1.log'))
-    #print "PPMC second 1 star review:" + str(ppmc(ANOTHER_ONE_STAR,4,'PPMC_1(2).log'))
-    print "PPMC first 2 star review:" + str(ppmc(TWO_STAR_REVIEW,4,'PPMC_2.log'))
-    #print "PPMC TEST:" + str(ppmc(TEST,4,'TEST.log'))
+    p = PPMC(4)
+    #print "PPMC first 5 stars review:" + str(p.compress(FIVE_STARS_TEXT,4,'PPMC_5.log'))
+    #print "PPMC second 5 stars review:" + str(p.compress(ANOTHER_FIVE_STAR,4,'PPMC_5(2).log'))
+    #print "PPMC first 1 star review:" + str(p.compress(ONE_STAR_TEXT,4,'PPMC_1.log'))
+    #print "PPMC second 1 star review:" + str(p.compress(ANOTHER_ONE_STAR,4,'PPMC_1(2).log'))
+    print "PPMC first 2 star review:" + str(p.compress(TWO_STAR_REVIEW,4,'PPMC_2.log'))
+    #print "PPMC TEST:" + str(p.compress(TEST,4,'TEST.log'))
 main()
