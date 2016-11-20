c622ef6e52ce4fb95cdadc72dfeb4f3ff6848b48
emariotti3 <eugenia.mariotti@live.com.ar>
2016-11-20 03:55:49 -0300

    Agrego correcciones a LSH y CarterWegman y prueba de integracion al notebook

--- TP_DATOS/LSH.py
+++ TP_DATOS/LSH.py
@@ -1,12 +1,15 @@
 from CWHashingFamily import CWHashingFamily
 
+BINS = 100
+PRIME = 127
+
 class LSH:
 
     def __init__(self, r, b):
         self.r = r
         self.b = b
         self.groupTables = [{} for i in xrange(0, b)]
-        self.lshFamily = CWHashingFamily()
+        self.lshFamily = CWHashingFamily(BINS, PRIME)
 
     def add(self, record):
         #Receives a record to be added to the LSH object.
@@ -20,11 +23,11 @@ class LSH:
             #to a different minhash function applied to the given record.
             minhashes = record.minhashes(i, self.b)
             pos = lshFunction.hash(minhashes)
-            if (groupHashTable.hasKey(pos)):
+            if (groupHashTable.has_key(pos)):
                 groupHashTable[pos].append(record.id())
             else:
                 groupHashTable[pos] = [record.id()]
-            
+        return record
 
     def getAllSimilarRecords(self, record):
         #Receives a query record.
@@ -41,6 +44,6 @@ class LSH:
             #to a different minhash function applied to the given record.
             minhashes = record.minhashes(i, self.b)
             pos = lshFunction.hash(minhashes)
-            if (groupHashTable.hasKey(pos)):
+            if (groupHashTable.has_key(pos)):
                 similarRecords += groupHashTable[pos]
         return similarRecords
