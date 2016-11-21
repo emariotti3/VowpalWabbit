1703c36bf61738ab5d89182b45675d6a53ad1732
czarnia <czarniana@gmail.com>
2016-11-21 13:44:15 -0300

    Uso de PPMC para prediccion

---
+++ TP_DATOS/BaseContext.py
@@ -0,0 +1,67 @@
+import logging
+
+END = 1
+BEGIN = 0
+CTX_NAME = "BASE CONTEXT"
+
+CHARACTERS = {" ":1,"a":1,"b":1,"c":1,"d":1,"e":1,"f":1,"g":1,"h":1,"i":1,"j":1,
+            "k":1,"l":1,"m":1,"n":1,"o":1,"p":1,"q":1,"r":1,"s":1,"t":1,"u":1,"v":1,
+            "w":1,"x":1,"y":1,"z":1, ".": 1, ",":1, "'":1, "!":1, "?":1,"-":1, "/":1,
+            "*":1, "+":1, ":":1, "%":1,"$":1, "#":1, "&":1,"(":1, ")":1, "=":1,">":1,
+            ";":1, "[":1, "]":1,"{":1, "}":1, "@":1, "|":1, "<":1, "_":1,
+            "0":1, "1":1, "2":1, "3":1, "4":1, "5":1, "6":1, "7":1, "8":1, "9":1}
+
+class BaseContext(object):
+
+    def __init__(self):
+        self.seenChars = CHARACTERS
+
+    def hasCharacter(self, character):
+        return character in self.seenChars.keys()
+
+    def getCharacterListWithExclusionPrinciple(self, contextList):
+        possibleChars = self.seenChars
+        for seenChar in self.seenChars.keys():
+            previouslySeen = False
+            previousContexts = contextList
+            while (not previouslySeen) and (previousContexts != []):
+                currentContext = previousContexts[0]
+                if (currentContext.hasCharacter(seenChar)):
+                    del possibleChars[seenChar]
+                    previouslySeen = True
+                previousContexts.pop(0)
+        return possibleChars
+
+    def calculateInterval(self, character, possibleChars, interval):
+        try:
+            totalFreq = 0
+            intervalLength = (interval[END] - interval[BEGIN])
+            logging.info("Old interval:( "+str(interval[BEGIN])+","+str(interval[END])+")")
+            for frequency in possibleChars.values():
+                totalFreq += frequency
+
+            if self.hasCharacter(character):
+                orderedChars = sorted(possibleChars.keys())
+                beginning = interval[BEGIN]
+
+                for currentChar in orderedChars:
+                    charProb = possibleChars[currentChar] / float(totalFreq)
+                    if (character == currentChar):
+                        end = beginning + intervalLength*charProb
+                        logging.info("New interval FOUND:("+str(beginning)+","+str(end)+")")
+                        return (beginning, end)
+                    else:
+                        beginning += intervalLength * charProb
+            else:
+                logging.exception("NO CHARACTER "+str(character)+" at " + CTX_NAME)
+                raise interval
+
+        except ZeroDivisionError:
+            logging.exception("Divided by zero at context: " + CTX_NAME + "." + str(e))
+            return interval
+
+
+    def compress(self, character, contextList, interval):
+        possibleChars = self.getCharacterListWithExclusionPrinciple(contextList)
+        newInterval = self.calculateInterval(character, possibleChars, interval)
+        return (True, newInterval)
---
+++ TP_DATOS/Model.py
@@ -0,0 +1,41 @@
+import logging
+from Context import Context
+
+class Model(object):
+
+    def __init__(self, number):
+        self.number = number
+        self.contexts = {}
+        self.toAdd = []
+
+    def __add(self, newContext):
+        self.toAdd.append(newContext)
+
+    def __refreshModel(self):
+        for i in xrange(0,len(self.toAdd)):
+            currContext = self.toAdd.pop(i)
+            if (not currContext in self.contexts):
+                self.contexts[currContext.name()] = currContext
+        self.toAdd = []
+
+    def compress(self, text, charIndex, previousContexts, interval):
+        self.__refreshModel()
+        if (charIndex >= self.number):
+            #In this case, the model can take the amount of characters
+            #indicated by self.number as context
+            character = text[charIndex]
+            logging.info( "MODEL #"+str(self.number))
+            currContext = text[charIndex] if (self.number == 0) else text[charIndex-self.number:charIndex]
+            logging.info( "compressing (" +str(currContext)+ ")" + str(character))
+            logging.info( "available contexts:" +str(self.contexts.keys()))
+
+            if (currContext in self.contexts):
+                return self.contexts[currContext].compress(character, previousContexts, interval)
+            elif (currContext != ""):
+                #This context does not exist in this model, so we add it.
+                self.__add(Context(currContext,character))
+                logging.info( "Added context:" + currContext + " to model "+str(self.number))
+
+        #This context could not compress this text because
+        #it does not contain the desired context.
+        return (False, interval)
